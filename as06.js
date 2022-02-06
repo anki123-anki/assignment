49e288fc4152041fd068b09527eed2c69ad7db04
anki123-anki <56147228+anki123-anki@users.noreply.github.com>
2022-02-06 20:50:41 +0530

    Add files via upload

---
+++ as06.js
@@ -0,0 +1,41 @@
+var obj = {
+    "first_name": "Aayush",
+    "last_name": "Sinha",
+    "email": "aayush.aryan@me.com",
+    "website": "sdfhsdhfjds",
+    "k": "This is Key K's value",
+    "students": [
+	{
+	    "name": "Jaydeep",
+	    "marks": 15
+	},
+	{
+	    "name": "Ankit",
+	    "marks": 17
+	},
+	{
+	    "name": "Aayush",
+	    "marks": 19
+	},
+	{
+	    "name": "Kabir",
+	    "marks": 21
+	},
+	{
+	    "name": "Akhil",
+	    "marks": 0
+	},
+        {
+	    "name": "Vikas",
+	    "marks": 27
+	},
+    ],
+};
+    
+    var num = parseInt(prompt("Enter a number"));
+    var studentData =  obj.students;
+    for(var i = 0;i < studentData.length; i++){
+              if(studentData[i].marks >= num){
+                  console.log(studentData[i].name);
+             }
+   }
