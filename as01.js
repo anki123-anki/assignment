30d5433a4fd07b99aa09141337a501f9765ddb86
anki123-anki <56147228+anki123-anki@users.noreply.github.com>
2022-02-06 20:15:30 +0530

    Add files via upload

---
+++ as01.js
@@ -0,0 +1,14 @@
+var a = parseInt(prompt("enter a first number"));
+       var b = parseInt(prompt("enter a second number"));
+       var c = parseInt(prompt("enter a third number"));
+
+       var min = 0;
+
+       if(a < b && a < c){
+       min = a;
+       }else if(b < a && b < c){
+       min = b;
+       }else{
+       min =  c;
+       }
+       alert(min);
