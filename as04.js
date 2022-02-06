7aafc1699cc9cd1087b7c5c39c2175abb47c7a18
anki123-anki <56147228+anki123-anki@users.noreply.github.com>
2022-02-06 20:42:41 +0530

    Add files via upload

---
+++ as04.js
@@ -0,0 +1,14 @@
+var  s = prompt("enter a string").split("");
+    var count = 0;   
+
+    for(var i=0;i<s.length;i++){
+         
+        if(s[i] == "a" || s[i] == "e" || s[i] == "i" || s[i] == "o" || s[i] == "u" ){
+            count++;
+        }
+        if(s[i] == "A" || s[i] == "E" || s[i] == "I" || s[i] == "O" || s[i] == "U"){
+            count++;
+        }
+    }
+
+    alert(count);
